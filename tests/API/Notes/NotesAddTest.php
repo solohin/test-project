b773504f2412c375e1213e34f7a1ae4d11dff0df
Elijah Solokhin <solohin.i@gmail.com>
2016-11-08 12:56:13 +0300

    Note add test

--- tests/API/Notes/NotesAddTest.php
+++ tests/API/Notes/NotesAddTest.php
@@ -9,33 +9,101 @@
 
 namespace Solohin\ToptalExam\Tests\API\Notes;
 
-use Silex\WebTestCase;
-use Solohin\ToptalExam\Services\UsersService;
-
 class NotesAddTest extends NotesTestTemplate
 {
     public function testAddByManager()
     {
-        //TODO implement
+        $responseData = $this->makeJsonRequest('/v1/notes', 'POST', 'manager', [
+            'text' => 'Second note',
+            'calories' => 200,
+            'user_id' => 2,
+            'time' => '14:00',
+            'date' => '02.01.2017',
+        ], false);
+
+        $debug = 'Response is ' . print_r($responseData, 1);
+        $this->assertEquals('permissions_error', $responseData['error_type'], $debug);
     }
 
     public function testAddByAdmin()
     {
-        //TODO implement
+        $startCalories = $this->caloriesSumm500Notes();
+        $this->makeJsonRequest('/v1/notes', 'POST', 'admin', [
+            'text' => 'Second note',
+            'calories' => 200,
+            'user_id' => 2,
+            'time' => '14:00',
+            'date' => '02.01.2017',
+        ], true);
+        $this->assertEquals($startCalories + 200, $this->caloriesSumm500Notes());
     }
 
-    public function testAddByUser()
+    public function testAddByAdminWithoutUser()
     {
-        //TODO implement
+        $responseData = $this->makeJsonRequest('/v1/notes', 'POST', 'admin', [
+            'text' => 'Second note',
+            'calories' => 200,
+            'time' => '14:00',
+            'date' => '02.01.2017',
+        ], false);
+        $this->assertEquals('empty_user_id', $responseData['error_type'], print_r($responseData, 1));
+    }
+
+    public function testAddByUserOwner()
+    {
+        $startCalories = $this->caloriesSumm500Notes();
+        $responseData = $this->makeJsonRequest('/v1/notes', 'POST', 'admin', [
+            'text' => 'Second note',
+            'calories' => 400,
+            'time' => '14:00',
+            'date' => '02.01.2017',
+            'user_id' => $this->dummyUsers['user']['token'],
+        ], true);
+        $this->assertEquals($startCalories + 400, $this->caloriesSumm500Notes());
+
+    }
+
+    public function testAddByUserNotOwner()
+    {
+        $userCalories = $this->caloriesSumm500Notes('user');
+
+        $this->makeJsonRequest('/v1/notes', 'POST', 'admin', [
+            'text' => 'Second note',
+            'calories' => 400,
+            'time' => '14:00',
+            'date' => '02.01.2017',
+            'user_id' => $this->dummyUsers['admin']['token'],
+        ], true);
+
+        //It should be added to user's account without error
+        $this->assertEquals($userCalories + 400, $this->caloriesSumm500Notes('user'));
     }
 
     public function testAddWrongDate()
     {
-        //TODO implement
+        $responseData = $this->makeJsonRequest('/v1/notes', 'POST', 'manager', [
+            'text' => 'Second note',
+            'calories' => 200,
+            'user_id' => 2,
+            'time' => '14:00',
+            'date' => '02-01-2017',
+        ], false);
+
+        $debug = 'Response is ' . print_r($responseData, 1);
+        $this->assertEquals('wrong_date', $responseData['error_type'], $debug);
     }
 
     public function testAddWrongTime()
     {
-        //TODO implement
+        $responseData = $this->makeJsonRequest('/v1/notes', 'POST', 'manager', [
+            'text' => 'Second note',
+            'calories' => 200,
+            'user_id' => 2,
+            'time' => 'twenty four sixty one',
+            'date' => '02.01.2017',
+        ], false);
+
+        $debug = 'Response is ' . print_r($responseData, 1);
+        $this->assertEquals('wrong_time', $responseData['error_type'], $debug);
     }
 }
--- tests/API/Notes/NotesTestTemplate.php
+++ tests/API/Notes/NotesTestTemplate.php
@@ -10,10 +10,86 @@
 namespace Solohin\ToptalExam\Tests\API\Notes;
 
 use Silex\WebTestCase;
+use Solohin\ToptalExam\Services\UsersService;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class NotesTestTemplate extends WebTestCase
 {
+    protected $dummyUsers = [
+        'user' => [
+            'username' => 'dummyUser',
+            'password' => 'Dummy Password 12345',
+            'roles' => ['ROLE_USER'],
+            'token' => 'old token',
+        ],
+        'admin' => [
+            'username' => 'dummyAdmin',
+            'password' => 'Dummy Password 12345',
+            'roles' => ['ROLE_ADMIN'],
+            'token' => 'old token',
+        ],
+        'manager' => [
+            'username' => 'dummyManager',
+            'password' => 'Dummy Password 12345',
+            'roles' => ['ROLE_MANAGER'],
+            'token' => 'old token',
+        ]
+    ];
+
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app['schema_manager']->flushDatabase();
+        $usersService = new UsersService($this->app['db']);
+
+        //Create users
+        $id = $usersService->insert($this->dummyUsers['user']);
+        $this->dummyUsers['user'] = $usersService->getOne($id);
+
+        $id = $usersService->insert($this->dummyUsers['admin']);
+        $this->dummyUsers['admin'] = $usersService->getOne($id);
+
+        $id = $usersService->insert($this->dummyUsers['manager']);
+        $this->dummyUsers['manager'] = $usersService->getOne($id);
+    }
+
+    public function tearDown()
+    {
+        //TODO FLUSH database
+        parent::tearDown();
+    }
+
+    protected function caloriesSumm500Notes($role = 'admin'){
+        $notes = $this->get500Notes($role);
+        return array_sum(array_column($notes, 'calories'));
+    }
+
+    protected function get500Notes($role = 'admin')
+    {
+        return $this->makeJsonRequest('/v1/notes', 'GET', $role, [], true)['notes'];
+    }
+
+    protected function makeJsonRequest($path, $type, $role, $data, $waitForOk)
+    {
+        $client = $this->createClient();
+
+        $client->request(strtoupper($type), $path, $data, [], [
+            'HTTP_X-AUTH-TOKEN' => $this->dummyUsers[$role]['token']
+        ]);
+
+        $response = $client->getResponse();
+        $rawResponse = $response->getContent();
+
+        $this->assertEquals($waitForOk, $client->getResponse()->isOk(), 'Response code is ' . $client->getResponse()->getStatusCode() . "\n" . 'Raw response is ' . $rawResponse);
+
+        $responseData = json_decode($rawResponse, 1);
+        $this->assertArrayHasKey('success', $responseData, $rawResponse);
+        $this->assertEquals($waitForOk, $responseData['success'], $rawResponse);
+
+        return $responseData;
+    }
 
     /**
      * Creates the application.
@@ -22,6 +98,8 @@ class NotesTestTemplate extends WebTestCase
      */
     public function createApplication()
     {
-        // TODO: Implement createApplication() method.
+        $app = require __DIR__ . '/../../../web/index.php';
+        unset($app['exception_handler']);
+        return $app;
     }
 }
