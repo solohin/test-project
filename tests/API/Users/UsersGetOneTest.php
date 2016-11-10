d160e21b97dad70e40e0d6c20d9e3bcef622a3ca
Elijah Solokhin <solohin.i@gmail.com>
2016-11-10 16:10:39 +0300

    Get one user tests

--- tests/API/Users/UsersGetOneTest.php
+++ tests/API/Users/UsersGetOneTest.php
@@ -13,24 +13,58 @@ class UsersGetOneTest extends UsersTestTemplate
 {
     public function testGetOneForManager()
     {
-        //TODO implement
+        $responseDataAdmin = $this->makeJsonRequest('/v1/users/2', 'get', 'manager', [], true)['user'];
+        $responseDataManager = $this->makeJsonRequest('/v1/users/3', 'get', 'manager', [], true)['user'];
+
+        $this->assertEquals('dummyAdmin', $responseDataAdmin['username']);
+        $this->assertEquals(['ROLE_ADMIN'], $responseDataAdmin['roles']);
+        $this->assertEquals(400, $responseDataAdmin['daily_normal']);
+        $this->assertEquals(false, $responseDataAdmin['can_edit']);
+        $this->assertArrayNotHasKey('token', $responseDataAdmin);
+        $this->assertArrayNotHasKey('password', $responseDataAdmin);
+
+        $this->assertEquals('dummyManager', $responseDataManager['username']);
+        $this->assertEquals(['ROLE_MANAGER'], $responseDataManager['roles']);
+        $this->assertEquals(400, $responseDataManager['daily_normal']);
+        $this->assertEquals(true, $responseDataManager['can_edit']);
+        $this->assertArrayNotHasKey('token', $responseDataAdmin);
+        $this->assertArrayNotHasKey('password', $responseDataAdmin);
     }
 
     public function testGetOneForAdmin()
     {
-        //TODO can edit property
-        //TODO implement
+        $responseDataAdmin = $this->makeJsonRequest('/v1/users/2', 'get', 'manager', [], true)['user'];
+        $responseDataManager = $this->makeJsonRequest('/v1/users/3', 'get', 'manager', [], true)['user'];
+
+        $this->assertEquals('dummyAdmin', $responseDataAdmin['username']);
+        $this->assertEquals(['ROLE_ADMIN'], $responseDataAdmin['roles']);
+        $this->assertEquals(400, $responseDataAdmin['daily_normal']);
+        $this->assertEquals(true, $responseDataAdmin['can_edit']);
+        $this->assertArrayNotHasKey('token', $responseDataAdmin);
+        $this->assertArrayNotHasKey('password', $responseDataAdmin);
+
+        $this->assertEquals('dummyManager', $responseDataManager['username']);
+        $this->assertEquals(['ROLE_MANAGER'], $responseDataManager['roles']);
+        $this->assertEquals(400, $responseDataManager['daily_normal']);
+        $this->assertEquals(true, $responseDataManager['can_edit']);
+        $this->assertArrayNotHasKey('token', $responseDataAdmin);
+        $this->assertArrayNotHasKey('password', $responseDataAdmin);
     }
 
     public function testGetOneForUserNotOwner()
     {
-        //permission denied
-        //TODO implement
+        $responseData = $this->makeJsonRequest('/v1/users/3', 'get', 'user', [], false);
+        $this->assertEquals('user_not_found', $responseData['error_type']);
     }
 
     public function testGetOneForUserOwner()
     {
-        //permission denied
-        //TODO implement
+        $responseData1 = $this->makeJsonRequest('/v1/users/1', 'get', 'user', [], true);
+        $responseDataMe = $this->makeJsonRequest('/v1/users/me', 'get', 'user', [], true);
+        $this->assertEquals($responseData1, $responseDataMe);
+
+        $this->assertArrayNotHasKey('token', $responseData1);
+        $this->assertArrayNotHasKey('password', $responseData1);
+        $this->assertEquals(true, $responseData1['can_edit']);
     }
 }
