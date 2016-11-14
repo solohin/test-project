3e1d5109b01866656f1a1a66d2b7d9326b59bb0e
Elijah Solokhin <solohin.i@gmail.com>
2016-11-14 13:37:59 +0300

    Change database credentials

--- config/prod.php
+++ config/prod.php
@@ -2,17 +2,10 @@
 $app['log.level'] = Monolog\Logger::ERROR;
 $app['api.version'] = "v1";
 $app['debug'] = false;
-//$app['db.options'] = array(
-//    "driver" => "pdo_mysql",
-//    "user" => "toptal",
-//    "password" => "O8z0rD61Z1yQDh38",
-//    "dbname" => "toptal",
-//    "host" => "127.0.0.1",
-//);
 $app['db.options'] = array(
     "driver" => "pdo_mysql",
-    "user" => "root",
-    "password" => "",
+    "user" => "toptal",
+    "password" => "O8z0rD61Z1yQDh38",
     "dbname" => "toptal",
     "host" => "127.0.0.1",
 );
